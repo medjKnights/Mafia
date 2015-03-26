11d4a714a1d3e59ced16f559188958744cba7964
MedjKnight001 <MedjKnight001>
2015-03-26 08:42:45 +0200

    26 03 2015

---
+++ Maf_Project/Maf_Project/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Maf_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.відкритиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.лічильникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.коректориToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справочнаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.містаТаФіліїToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проПрограмуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.азазаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.відкритиToolStripMenuItem,
+            this.справочнаToolStripMenuItem,
+            this.справкаToolStripMenuItem,
+            this.азазаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(625, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // відкритиToolStripMenuItem
+            // 
+            this.відкритиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.лічильникиToolStripMenuItem,
+            this.коректориToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.вихідToolStripMenuItem});
+            this.відкритиToolStripMenuItem.MergeIndex = 0;
+            this.відкритиToolStripMenuItem.Name = "відкритиToolStripMenuItem";
+            this.відкритиToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.відкритиToolStripMenuItem.Text = "Відкрити";
+            // 
+            // лічильникиToolStripMenuItem
+            // 
+            this.лічильникиToolStripMenuItem.Name = "лічильникиToolStripMenuItem";
+            this.лічильникиToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.лічильникиToolStripMenuItem.Text = "Гравці";
+            this.лічильникиToolStripMenuItem.Click += new System.EventHandler(this.лічильникиToolStripMenuItem_Click);
+            // 
+            // коректориToolStripMenuItem
+            // 
+            this.коректориToolStripMenuItem.Name = "коректориToolStripMenuItem";
+            this.коректориToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.коректориToolStripMenuItem.Text = "Ігри";
+            this.коректориToolStripMenuItem.Click += new System.EventHandler(this.коректориToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(106, 6);
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            this.вихідToolStripMenuItem.Click += new System.EventHandler(this.вихідToolStripMenuItem_Click);
+            this.вихідToolStripMenuItem.MouseEnter += new System.EventHandler(this.вихідToolStripMenuItem_MouseEnter);
+            this.вихідToolStripMenuItem.MouseLeave += new System.EventHandler(this.вихідToolStripMenuItem_MouseLeave);
+            // 
+            // справочнаToolStripMenuItem
+            // 
+            this.справочнаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.містаТаФіліїToolStripMenuItem});
+            this.справочнаToolStripMenuItem.MergeIndex = 1;
+            this.справочнаToolStripMenuItem.Name = "справочнаToolStripMenuItem";
+            this.справочнаToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.справочнаToolStripMenuItem.Text = "Справочна";
+            // 
+            // містаТаФіліїToolStripMenuItem
+            // 
+            this.містаТаФіліїToolStripMenuItem.Name = "містаТаФіліїToolStripMenuItem";
+            this.містаТаФіліїToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.містаТаФіліїToolStripMenuItem.Text = "Графік проведення";
+            this.містаТаФіліїToolStripMenuItem.Visible = false;
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.проПрограмуToolStripMenuItem});
+            this.справкаToolStripMenuItem.MergeIndex = 2;
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // проПрограмуToolStripMenuItem
+            // 
+            this.проПрограмуToolStripMenuItem.Name = "проПрограмуToolStripMenuItem";
+            this.проПрограмуToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.проПрограмуToolStripMenuItem.Text = "Про програму";
+            this.проПрограмуToolStripMenuItem.Click += new System.EventHandler(this.проПрограмуToolStripMenuItem_Click);
+            // 
+            // азазаToolStripMenuItem
+            // 
+            this.азазаToolStripMenuItem.Name = "азазаToolStripMenuItem";
+            this.азазаToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.азазаToolStripMenuItem.Text = "азаза1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 297);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(625, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 319);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "АІС \"Мафія\"";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem відкритиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справочнаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проПрограмуToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem лічильникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem коректориToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem містаТаФіліїToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem азазаToolStripMenuItem;
+    }
+}
+
---
+++ Maf_Project/Maf_Project/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Maf_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int person;
+        string posada = "user not found";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+       
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void вихідToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+        {
+            //toolStripStatusLabel1.Text = "Вихід із програми";
+        }
+
+        private void вихідToolStripMenuItem_MouseLeave(object sender, EventArgs e)
+        {
+            //toolStripStatusLabel1.Text = "";
+        }
+
+        private void проПрограмуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //AboutBox1 about = new AboutBox1();
+            //about.Show();
+        }
+
+        private void доступДоАІСToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form_1 FForm = new Form_1();
+            //FForm.MdiParent = this;
+            //FForm.Text = (sender as ToolStripMenuItem).Text;
+            //FForm.posada = posada;
+            //FForm.Show();
+            
+        }
+
+        private void доступДоАІСToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+        {
+            //toolStripStatusLabel1.Text = "Відкрити форму для роботи з таблицею " + "'" + (sender as ToolStripMenuItem).Text + "'";
+        }
+
+        private void доступДоАІСToolStripMenuItem_MouseLeave(object sender, EventArgs e)
+        {
+            //toolStripStatusLabel1.Text = "";
+        }
+
+        private void logInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //LoginForm LForm = new LoginForm();
+            //LForm.ShowDialog();
+            //posada = LForm.posada;
+            //userToolStripMenuItem.Text = posada;
+            //if (posada != "user not found")
+            //{
+            //    logInToolStripMenuItem.Enabled = false;
+            //    logOutToolStripMenuItem.Enabled = true;
+            //    posadaUpDate(posada);
+            //    person = LForm.person;
+            //}
+
+        }
+
+        private void userToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //posada = "user not found";
+            //userToolStripMenuItem.Text = posada;
+            //logInToolStripMenuItem.Enabled = true;
+            //logOutToolStripMenuItem.Enabled = false;
+            //posadaUpDate(posada);
+            //for(int i = 0;i<MdiChildren.Count();i++)
+            //MdiChildren.ElementAt(i).Close();
+        }
+
+        private void лічильникиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form_4 FForm = new Form_4();
+            //FForm.MdiParent = this;
+            //FForm.Text = (sender as ToolStripMenuItem).Text;
+            //FForm.posada = posada;
+            //FForm.person = person;
+            //FForm.Show();
+        }
+
+        private void графікПовірокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form_3 FForm = new Form_3();
+            //FForm.MdiParent = this;
+            //FForm.Text = (sender as ToolStripMenuItem).Text;
+            //FForm.posada = posada;
+            //FForm.Show();
+        }
+
+        private void коректориToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form_2 FForm = new Form_2();
+            //FForm.MdiParent = this;
+            //FForm.Text = (sender as ToolStripMenuItem).Text;
+            //FForm.person = person;
+            //FForm.posada = posada;
+            //FForm.Show();
+        }
+
+    }
+}
